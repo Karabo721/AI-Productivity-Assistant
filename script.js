b8e8290f9e71f8cbbf32495a8e47c902c8fea1ad
Karabo721 <karaboboitumelo721@gmail.com>
2026-05-21 14:06:34 +0200

    Create script.js
    Added email generator functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+function generateEmail() {
+
+    let purpose = document.getElementById("purpose").value;
+    let tone = document.getElementById("tone").value;
+
+    let email = "";
+
+    if (tone === "Formal") {
+        email = "Dear Sir/Madam,<br><br>I hope you are well. I am writing regarding " + purpose + ".<br><br>Kind regards.";
+    }
+
+    else if (tone === "Friendly") {
+        email = "Hi there,<br><br>Hope you're doing great! I'm reaching out about " + purpose + ".<br><br>Best wishes.";
+    }
+
+    else {
+        email = "Hello,<br><br>I would like to discuss " + purpose + " and explore possible opportunities.<br><br>Thank you.";
+    }
+
+    document.getElementById("output").innerHTML = email;
+}
